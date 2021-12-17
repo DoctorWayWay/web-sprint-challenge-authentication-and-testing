46791d9cecf8ee98fc64a09df870e97daf39e5cf
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-17 14:12:14 -0800

    Forced returns in auth middlewares

--- api/middleware/auth-middleware.js
+++ api/middleware/auth-middleware.js
@@ -4,14 +4,14 @@ const bcrypt = require('bcryptjs')
 async function validateRegister(req, res, next) {
   const { username, password } = req.body
   if (!username || !password) {
-    next({
+    return next({
       status: 400,
       message: "username and password required"
     })
   }
   const foundUser = await Users.findBy({ username: username })
   if (foundUser) {
-    next({
+    return next({
       status: 409,
       message: "username taken"
     })
@@ -22,14 +22,14 @@ async function validateRegister(req, res, next) {
 const validateLogin = async (req, res, next) => {
   const { username, password } = req.body
   if (!username || !password) {
-    next({
+    return next({
       status: 400,
       message: "username and password required"
     })
   }
   const existingUser = await Users.findBy({ username })
   if (!existingUser || bcrypt.compareSync(password, existingUser.password) === false) {
-    next({
+    return next({
       status: 401,
       message: "invalid credentials"
     })
