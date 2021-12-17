c0c5946fde58aa67ed76768727e560e4718b13cc
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-17 14:08:10 -0800

    Changed user model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,9 +1,8 @@
 const db = require("../../data/dbConfig")
 
 async function findBy(filter) {
-  const foundUser = await db("users")
+  const [foundUser] = await db("users")
     .where(filter)
-    .first()
   return foundUser
 }
 
